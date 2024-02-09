de860bad79da3a0d844191c8a49d81ceb912314c
Ayanqq <nazbievayan16@gmail.com>
2024-02-09 15:58:15 +0600

    updated menu styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,6 +4,7 @@ import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {HeaderMenu} from "./headerMenu/HeaderMenu";
 
 const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
 
@@ -14,7 +15,7 @@ export const Header = () => {
             <Container>
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
-                    <Menu menuItems={items}/>
+                    <HeaderMenu menuItems={items}/>
                 </FlexWrapper>
             </Container>
         </StyledHeader>
@@ -25,5 +26,12 @@ export const Header = () => {
 
 
 const StyledHeader = styled.header`
-  background-color: #e9daff;
+  background-color: rgba(31, 31, 32, 0.9);
+  padding:10px 0;
+  position:fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 99999;
+  box-shadow: 0 0 74px -10px #7572D5;
 `
