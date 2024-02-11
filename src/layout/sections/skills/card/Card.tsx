39a673032c58da9c8406d45c30c987a6fe357b96
Ayanqq <nazbievayan16@gmail.com>
2024-02-11 18:15:30 +0600

    added styles to Skills.tsx pt.2

--- src/layout/sections/skills/card/Card.tsx
+++ src/layout/sections/skills/card/Card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
 import {theme} from "../../../../styles/Theme";
+import {Icon} from "../../../../components/icon/Icon";
 
 
 type CardPropsType = {
@@ -15,41 +16,75 @@ export const Card = (props: CardPropsType) => {
             <StyledText>
                 <DescriptionCard>{props.description}</DescriptionCard>
                 <TitleCard>{props.title}</TitleCard>
-                <a href="">Learn More</a>
+
+                <LinkWrapper>
+                    <Link href="">Learn More</Link>
+                    <Icon iconSrc={"arrowSvg"}/>
+                </LinkWrapper>
             </StyledText>
 
-            <img src={props.imgSrc} alt=""/>
+            <PhotoWrapper>
+                <StyledImg src={props.imgSrc} alt=""/>
+            </PhotoWrapper>
         </StyledCard>
     );
 };
 
 
 const StyledCard = styled.div`
-  border:3px solid ${theme.colors.accent};
+  border: 3px solid ${theme.colors.accent};
   width: 100%;
   border-radius: 30px;
   display: flex;
   justify-content: space-around;
+  padding: 0 51px;
 
+`
+const LinkWrapper = styled.div`
+  display: flex;
 
-  img {
-    width: 554px;
-    height: 394px;
-    object-fit: cover;
-    border-radius: 10px;
-  }
 `
 
+const Link = styled.a`
+  padding-right: 26px;
+  font-size: 20px;
+  font-weight: 500;
+  color: white;
+`
+
+
 const DescriptionCard = styled.span`
+  color: grey;
+  font-size: 16px;
+  font-weight: 400;
+  text-transform: uppercase;
+
 `
 
 const TitleCard = styled.h2`
   max-width: 380px;
+  font-size: 35px;
+  font-weight: 700;
+  text-align: left;
 `
 
 const StyledText = styled.div`
-    display:flex;
-    flex-direction:column;
-    justify-content: space-around;
-    
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
 `
+
+const StyledImg = styled.img`
+  border-radius: 20px;
+  width: 555px;
+  height: 395px;
+
+
+`
+
+const PhotoWrapper = styled.div`
+`
+// 1. Как сделать отступ между карточками
+// 2. Не поднимается вверх карточка
+// 3. Как обращаться к элементу по-середине, через псевдоклассы ?
